2fc19c956e481fabea943776828aaf86c5376330
Hassannaveed10 <44541924+Hassannaveed10@users.noreply.github.com>
2019-01-24 14:46:39 +0500

    Add files via upload
    quiz 8

--- pro/index.php
+++ pro/index.php
@@ -1,8 +1,7 @@
-<?php
-    require "php/func.php";
-?>
-
 <!DOCTYPE html>
+<?php
+require "server/functions.php";
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -12,6 +11,20 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Old+Standard+TT">
+    <script>
+        function checkDatabase(str) {
+             {
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        document.getElementById("show").innerHTML = this.responseText;
+                    }
+                };
+                xmlhttp.open("GET", "check_product.php?e=" + str, true);
+                xmlhttp.send();
+            }
+        }
+    </script>
 </head>
 <body>
 
@@ -31,10 +44,10 @@
                         <form class="form-inline">
                             <div class="input-group">
                                 <input type="search" class="form-control"
-                                       id="search-bar" name="search"
+                                       id="search-bar" name="search" onkeyup="checkDatabase(this.value)"
                                        placeholder="Find Mobile Phones, Laptops, and more..">
                                 <div class="input-group-append">
-                                    <button class="btn btn-outline-secondary btn-lg" type="button"><i class="fas fa-search"></i></button>
+                                    <button class="btn btn-outline-secondary btn-lg" type="submit"><i class="fas fa-search"></i></button>
                                 </div>
                             </div>
                         </form>
@@ -42,13 +55,13 @@
                     <div class="col-lg-3 col-md-4 col-sm-5">
                         <ul class="navbar-nav mr-auto mt-2 mt-lg-0 float-sm-right">
                             <li class="nav-item active">
-                                <a class="nav-link" href="index.php"><i class="fas fa-heart sc-color fa-2x"></i></a>
+                                <a class="nav-link" href="index.html"><i class="fas fa-heart sc-color fa-2x"></i></a>
                             </li>
                             <li class="nav-item active">
-                                <a class="nav-link" href="index.php"><i class="fas fa-shopping-cart sc-color fa-2x"></i></a>
+                                <a class="nav-link" href="index.html"><i class="fas fa-shopping-cart sc-color fa-2x"></i></a>
                             </li>
                             <li class="nav-item active">
-                                <a class="nav-link" href="index.php"> <span class="sc-fs">Login </span></a>
+                                <a class="nav-link" href="index.html"> <span class="sc-fs">Login </span></a>
                             </li>
                         </ul>
                     </div>
@@ -78,18 +91,6 @@
                 </a>
                 <ul class="collapse show list-unstyled" id="pageSubmenu">
                     <?php getBrands(); ?>
-<!--                    <li>
-                        <a class="nav-link" href="#">Apple</a>
-                    </li>
-                    <li>
-                        <a class="nav-link" href="#">Sony</a>
-                    </li>
-                    <li>
-                        <a class="nav-link" href="#">Dell</a>
-                    </li>
-                    <li>
-                        <a class="nav-link" href="#">Toshiba</a>
-                    </li>-->
                 </ul>
             </li>
             <li>
@@ -108,10 +109,8 @@
     </nav>
     <article id="content" class="container-fluid bg-white">
 
-        <div class="row">
-            <div class="col">
-                Content
-            </div>
+        <div class="row" id="show">
+                <?php getPro(); ?>
         </div>
     </article>
 
